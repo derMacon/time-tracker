ba7193702af7668090198a4322a5ec55088798fc
derMacon <silash169@gmail.com>
2020-07-22 04:03:07 +0200

    centered columns

--- project/src/main/java/com/dermacon/timeTracker/TerminalUI.java
+++ project/src/main/java/com/dermacon/timeTracker/TerminalUI.java
@@ -10,38 +10,50 @@ import java.util.TimerTask;
 public class TerminalUI implements UserInterface {
 
     private static final String INTRO = "time-tracker v1.0\n";
+    private static final String MANUAL = "usage:\n" +
+            "  - p: pause (wip)\n" +
+            "  - q: quit without editing\n" +
+            "  - e: quit with editing\n";
 
     private static final int FST_COL_WIDTH = 5;
-    private static final int SND_COL_WIDTH = 10;
-    private static final int THRD_COL_WIDTH = 10;
-    private static final String LINE_FORMAT = "|%1$-" + FST_COL_WIDTH +
-            "s|%2$-10s|%3$-20s|\n";
+    private static final int SND_COL_WIDTH = 20;
+    private static final int THRD_COL_WIDTH = 20;
+
+    private static final String LINE_FORMAT = "|%1$-" + FST_COL_WIDTH + "s"
+            + "|%2$-" + SND_COL_WIDTH + "s"
+            + "|%3$-" + THRD_COL_WIDTH + "s|\n";
     private static final int DISPLAY_INTERVAL = 1000;
 
     private Timer t = new Timer(true);
 
     @Override
     public TrackingTask selectTask() {
-        StringBuilder options = new StringBuilder(INTRO);
-        options.append("select tracking task:\n");
+        StringBuilder options = new StringBuilder(INTRO + "\n");
 
-        options.append(System.out.format(LINE_FORMAT,
-                StringUtils.center("idx",10),
-                StringUtils.center("name",10),
-                StringUtils.center("duration",20)));
+        options.append(String.format(LINE_FORMAT,
+                StringUtils.center("idx",FST_COL_WIDTH),
+                StringUtils.center("name",SND_COL_WIDTH),
+                StringUtils.center("duration",THRD_COL_WIDTH)));
 
+        options.append(getHoriLine());
 
         List<File> trackedFiles = FileHandler.getTrackedFiles();
         int option_cnt = trackedFiles.size();
 
         for (int i = 0; i < option_cnt; i++) {
-            options.append(System.out.format(LINE_FORMAT,
-                    StringUtils.center(i + "",10),
-                    StringUtils.center(trackedFiles.get(i).getName(),10),
-                    StringUtils.center("-",20)));
+            options.append(String.format(LINE_FORMAT,
+                    StringUtils.center((1 + i) + "",FST_COL_WIDTH),
+                    StringUtils.center(trackedFiles.get(i).getName(),SND_COL_WIDTH),
+                    StringUtils.center("-",THRD_COL_WIDTH)));
         }
-        options.append((option_cnt + 1) + ". new tracking task\n");
-        System.out.println(options);
+
+        options.append(String.format(LINE_FORMAT,
+                StringUtils.center((option_cnt + 1) + "",FST_COL_WIDTH),
+                StringUtils.center("new task",SND_COL_WIDTH),
+                StringUtils.center("-",THRD_COL_WIDTH)));
+
+        options.append("\nuser input: ");
+        System.out.print(options);
 
         String userInput;
         Scanner s = new Scanner(System.in);
@@ -54,18 +66,40 @@ public class TerminalUI implements UserInterface {
     }
 
 
+    private String getHoriLine() {
+        StringBuilder out = new StringBuilder("+");
+        int line_width = FST_COL_WIDTH + SND_COL_WIDTH + THRD_COL_WIDTH;
+        for (int i = 1; i <= line_width; i++) {
+            if (i == FST_COL_WIDTH
+                    || i == FST_COL_WIDTH + SND_COL_WIDTH
+                    || i == FST_COL_WIDTH + SND_COL_WIDTH + THRD_COL_WIDTH
+            ) {
+                out.append("-+");
+            } else {
+                out.append("-");
+            }
+        }
+        out.append("\n");
+        return out.toString();
+    }
+
+
+
     @Override
     public void waitForUserAbortion(TrackingTask task) {
+
+        System.out.println("\n" + MANUAL);
+
         Scanner s = new Scanner(System.in);
         String userInteraction;
         do {
             userInteraction = s.next();
-        } while (!userInteraction.equals("q") && !userInteraction.equals("q!"));
+        } while (!userInteraction.equals("q") && !userInteraction.equals("e"));
 
         task.stopTask();
 
         // user wants to edit ending time
-        if (userInteraction.equals("q")) {
+        if (userInteraction.equals("e")) {
             editEndingTime(task);
         }
 
@@ -93,13 +127,13 @@ public class TerminalUI implements UserInterface {
             @Override
             public void run() {
                 if (task.isRunning()) {
-                    System.out.print("[" + task.displayPassedTime() + "] | " +
-                            "user input>\r");
+                    System.out.print("user input [" + task.displayPassedTime()
+                            + "] > \r");
                 } else {
                     System.out.print("task not running\r");
                 }
             }
-        }, 0, DISPLAY_INTERVAL);
+        }, 10, DISPLAY_INTERVAL);
     }
 
 
